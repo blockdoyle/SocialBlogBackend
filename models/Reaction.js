e9963157da7c891cdaf4dd61cd124a7f6dd26214
Brock Lockhart-Doyle <blockdoyle@gmail.com>
2024-03-26 00:49:46 -0400

    created models for User,Thought,Reaction

---
+++ models/Reaction.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const reactionSchema = new mongoose.Schema({
+  reactionId: {
+    type: mongoose.Schema.Types.ObjectId,
+    default: () => new Types.ObjectId(),
+  },
+  reactionBody: {
+    type: String,
+    required: true,
+    maxlength: 280,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    // TODO: Use a getter method to format the timestamp on query
+  },
+});
---
+++ models/Thought.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const thoughtSchema = new mongoose.Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 280,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    // TODO: Use a getter method to format the timestamp on query
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  reactions: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Reaction",
+    },
+  ],
+});
+
+const Thought = mongoose.model("Thought", thoughtSchema);
+
+module.exports = Thought;
