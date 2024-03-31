e1429816af3a436bbbc64a1f27a7270c35373131
Brock Lockhart-Doyle <blockdoyle@gmail.com>
2024-03-31 14:54:26 -0400

    Enable routes in server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const db = require("./config/connection");
-// const routes = require("./routes");
+const routes = require("./routes");
 
 // Create a new Express application
 const PORT = process.env.PORT || 3001;
