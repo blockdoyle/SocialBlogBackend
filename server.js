6bf4c1f8ab044c4f29a0fb743b16035adb772146
Brock Lockhart-Doyle <blockdoyle@gmail.com>
2024-03-31 12:27:39 -0400

    Remove unused code and comments

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
 const express = require("express");
 const db = require("./config/connection");
+// const routes = require("./routes");
 
-// import models
-
+// Create a new Express application
 const PORT = process.env.PORT || 3001;
 const app = express();
-
-// Routes
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
 
 // Start the server
 db.once("open", () => {
