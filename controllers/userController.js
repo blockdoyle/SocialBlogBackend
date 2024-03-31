5792f8d5b70911026fa5ad1475413836eb6af5de
Brock Lockhart-Doyle <blockdoyle@gmail.com>
2024-03-31 14:32:44 -0400

    Add userController.js with getUsers and getSingleUser functions

---
+++ controllers/userController.js
@@ -0,0 +1,43 @@
+const { ObjectId } = require("mongodb").Types;
+const { User, Thought, Reaction } = require("../models");
+
+module.exports = {
+  // Get all users
+  async getUsers(req, res) {
+    try {
+      const users = await User.find();
+
+      const userObj = {
+        users,
+      };
+
+      res.json(userObj);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+
+  // Get a single user by its _id and populated thought and friend data
+  async getSingleUser(req, res) {
+    try {
+      const user = await User.findOne({ _id: req.params.userId });
+
+      if (!user) {
+        return res.status(404).json({ message: "No user found with this id!" });
+      }
+
+      // Create an object to store the user, its thoughts, and its friends
+      const userObj = {
+        user,
+        thoughts: [],
+        friends: [],
+      };
+
+      res.json(userObj);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+};
