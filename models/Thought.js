54ec7470d2d0058edf293fc499e31aa8ae04defe
Brock Lockhart-Doyle <blockdoyle@gmail.com>
2024-04-01 14:06:49 -0400

    Created virtual to format current time

--- models/Thought.js
+++ models/Thought.js
@@ -2,33 +2,41 @@ const mongoose = require("mongoose");
 const moment = require("moment");
 
 // Schema design for the Thought model
-const thoughtSchema = new mongoose.Schema({
-  thoughtText: {
-    type: String,
-    required: true,
-    minlength: 1,
-    maxlength: 280,
-  },
-  createdAt: {
-    type: Date,
-    default: getTimestamp(Date.now()),
-  },
-  username: {
-    type: String,
-    required: true,
-  },
-  reactions: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Reaction",
+const thoughtSchema = new mongoose.Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
     },
-  ],
-});
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Reaction",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      // getters: true,
+    },
+  }
+);
 
-// getter function to get the converted timestamp.
-const getTimestamp = (time) => {
-  return moment(time).format("MMM Do, YYYY [at] hh:mm a");
-};
+// virtual getter function to get the converted timestamp.
+thoughtSchema.virtual("formattedCreatedAt").get(function () {
+  return moment(this.createdAt).format("MMM Do, YYYY [at] hh:mm a");
+});
 
 const Thought = mongoose.model("Thought", thoughtSchema);
 
