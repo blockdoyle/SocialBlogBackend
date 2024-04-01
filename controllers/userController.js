2ae625601c19b9e39ca33d492a33dc044d82e9e9
Brock Lockhart-Doyle <blockdoyle@gmail.com>
2024-04-01 15:20:24 -0400

    Add createUser, updateUser, and deleteUser functions to userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -41,10 +41,47 @@ module.exports = {
     }
   },
 
+  // POST request to create a new user
   async createUser(req, res) {
     console.log(req.body);
     try {
       const user = await User.create(req.body);
+
+      res.json(user);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+
+  // PUT request to update a user by its _id
+  async updateUser(req, res) {
+    try {
+      const user = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $set: req.body },
+        { runValidators: true, new: true }
+      );
+
+      if (!user) {
+        return res.status(404).json({ message: "No user found with this id!" });
+      }
+
+      res.json(user);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+
+  // DELETE request to remove a user by its _id
+  async deleteUser(req, res) {
+    try {
+      const user = await User.findOneAndDelete({ _id: req.params.userId });
+
+      if (!user) {
+        return res.status(404).json({ message: "No user found with this id!" });
+      }
 
       res.json(user);
     } catch (err) {
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -3,6 +3,8 @@ const {
   getUsers,
   getSingleUser,
   createUser,
+  updateUser,
+  deleteUser,
 } = require("../../controllers/userController");
 
 // GET all users at /api/users
@@ -13,5 +15,11 @@ router.route("/:userId").get(getSingleUser);
 
 // POST request to create a new user at /api/users
 router.route("/").post(createUser);
+
+// PUT request to update a user by its _id at /api/users/:userId
+router.route("/:userId").put(updateUser);
+
+// DELETE request to remove a user by its _id at /api/users/:userId
+router.route("/:userId").delete(deleteUser);
 
 module.exports = router;
